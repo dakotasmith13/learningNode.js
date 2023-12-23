544eecb799d6a4527f0f02ad98d8cdd95dcf5008
BuildTools <dakota.smith@uvm.edu>
2023-12-23 17:59:45 -0500

    Module 4 Complete

--- module_4-event_emitter/logEvents.js
+++ module_4-event_emitter/logEvents.js
@@ -7,13 +7,14 @@ const path = require('path');
 
 const logEvents = async (message) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
-    const logItem = `${dateTime}\t${uuid()}\t${message}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
     console.log(logItem);
 
     try {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, 'logs'));
         }
+        // testing
         await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
     } catch (err) {
         console.log(err);
