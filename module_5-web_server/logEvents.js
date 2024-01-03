38b646e8d9a6ad388aa79991b36e8eeee753a2bc
BuildTools <dakota.smith@uvm.edu>
2024-01-02 21:19:22 -0500

    Module 5 - finished creating web server

--- module_5-web_server/logEvents.js
+++ module_5-web_server/logEvents.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const logEvents = async (message) => {
+const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
     console.log(logItem);
@@ -15,7 +15,7 @@ const logEvents = async (message) => {
             await fsPromises.mkdir(path.join(__dirname, 'logs'));
         }
         // testing
-        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
     } catch (err) {
         console.log(err);
     }
--- module_5-web_server/server.js
+++ module_5-web_server/server.js
@@ -1,18 +1,117 @@
-const { EventEmitter } = require('stream');
-const logEvents = require('./logEvents');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
 
-const eventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {}; 
-
+const { EventEmitter } = require("stream");
+const logEvents = require("./logEvents");
+const eventEmitter = require("events");
+class Emitter extends EventEmitter {}
 // initialize objects
-const myEmitter = new MyEmitter();
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+
+const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes("image") ? "utf8" : ""
+    );
+    const data =
+      contentType === "application/json" ? JSON.parse(rawData) : rawData;
+    response.writeHead(
+        filePath.includes('404.html') ? 404 : 200, 
+        { "Content-Type": contentType });
+    response.end(
+      contentType === "application/json" ? JSON.stringify(data) : data
+    );
+  } catch (err) {
+    console.log(err);
+    myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+    response.statusCode = 500;
+    response.end();
+  }
+};
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
+  const extension = path.extname(req.url);
+  let contentType;
+
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".txt":
+      contentType = "text/plain";
+      break;
+    default:
+      contentType = "text/html";
+  }
+
+  let filePath =
+    contentType === "text/html" && req.url === "/"
+      ? path.join(__dirname, "views", "index.html")
+      : contentType === "text/html" && req.url.slice(-1) === "/"
+      ? path.join(__dirname, "views", req.url, "index.html")
+      : contentType === "text/html"
+      ? path.join(__dirname, "views", req.url)
+      : path.join(__dirname, req.url);
+
+  // makes the .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    // serve the file
+    serveFile(filePath, contentType, res);
+  } else {
+    // 404
+    // 301 - redirect
+    switch (path.parse(filePath).base) {
+      case "old-page.html":
+        res.writeHead(301, { Location: "/new-page.html" });
+        res.end();
+        break;
+      case "www-page.html":
+        res.writeHead(301, { Location: "/" });
+        res.end();
+        break;
+      default:
+        // serve a 404 response
+        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
+    }
+  }
+
+  //   let filePath;
+
+  // if (req.url === '/' || req.url === 'index.html') {
+  //     res.statusCode = 200;
+  //     res.setHeader('Content-Type', 'text/html');
+  //     filePath = path.join(__dirname, 'views', 'index.html');
+  //     fs.readFile(filePath, 'utf8', (err, data) => {
+  //         res.end(data);
+  //     });
+  // }
+});
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
 // add listener for log event
-myEmitter.on('log', (msg) => logEvents(msg));
-
-setTimeout(() => {
-    // Emit event
-    myEmitter.emit('log', 'Log event emitted!');
-
-}, 2000)
