06667e97d0cb0aaf46ff16624c2b9fb0e7ec8795
BuildTools <dakota.smith@uvm.edu>
2024-01-02 22:03:18 -0500

    Module 6 - created express server and learned route handlers

--- module_6-express-js/server.js
+++ module_6-express-js/server.js
@@ -1,117 +1,54 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 const path = require("path");
-const fs = require("fs");
-const fsPromises = require("fs").promises;
-
-const { EventEmitter } = require("stream");
-const logEvents = require("./logEvents");
-const eventEmitter = require("events");
-class Emitter extends EventEmitter {}
-// initialize objects
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
-
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-  try {
-    const rawData = await fsPromises.readFile(
-      filePath,
-      !contentType.includes("image") ? "utf8" : ""
-    );
-    const data =
-      contentType === "application/json" ? JSON.parse(rawData) : rawData;
-    response.writeHead(
-        filePath.includes('404.html') ? 404 : 200, 
-        { "Content-Type": contentType });
-    response.end(
-      contentType === "application/json" ? JSON.stringify(data) : data
-    );
-  } catch (err) {
-    console.log(err);
-    myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
-    response.statusCode = 500;
-    response.end();
-  }
-};
-
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
-
-  const extension = path.extname(req.url);
-  let contentType;
-
-  switch (extension) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".jpg":
-      contentType = "image/jpeg";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".txt":
-      contentType = "text/plain";
-      break;
-    default:
-      contentType = "text/html";
-  }
-
-  let filePath =
-    contentType === "text/html" && req.url === "/"
-      ? path.join(__dirname, "views", "index.html")
-      : contentType === "text/html" && req.url.slice(-1) === "/"
-      ? path.join(__dirname, "views", req.url, "index.html")
-      : contentType === "text/html"
-      ? path.join(__dirname, "views", req.url)
-      : path.join(__dirname, req.url);
-
-  // makes the .html extension not required in the browser
-  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
-
-  const fileExists = fs.existsSync(filePath);
-
-  if (fileExists) {
-    // serve the file
-    serveFile(filePath, contentType, res);
-  } else {
-    // 404
-    // 301 - redirect
-    switch (path.parse(filePath).base) {
-      case "old-page.html":
-        res.writeHead(301, { Location: "/new-page.html" });
-        res.end();
-        break;
-      case "www-page.html":
-        res.writeHead(301, { Location: "/" });
-        res.end();
-        break;
-      default:
-        // serve a 404 response
-        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
-    }
-  }
-
-  //   let filePath;
-
-  // if (req.url === '/' || req.url === 'index.html') {
-  //     res.statusCode = 200;
-  //     res.setHeader('Content-Type', 'text/html');
-  //     filePath = path.join(__dirname, 'views', 'index.html');
-  //     fs.readFile(filePath, 'utf8', (err, data) => {
-  //         res.end(data);
-  //     });
-  // }
+app.get("^/$|/index(.html)?", (req, res) => {
+  // begin w/ a slash and end with a slash, or index.html
+  // res.sendFile('./views/index.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
 
-// add listener for log event
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html"); // sends 302 by default, we want 301 (permanent)
+});
+
+// Route handlers (req, res) anon function are route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next(); // moves on to next handler
+  },
+  (req, res) => {
+    res.send("Hellow world!");
+  }
+);
+
+// chaining route handlers
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+}
+
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+}
+
+const three = (req, res) => {
+  console.log("three");
+  res.send("Finished!");
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
