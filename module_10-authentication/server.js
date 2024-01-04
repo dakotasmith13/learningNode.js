591dc00cb6aa0a7a8829ab0320347e07db405ac7
BuildTools <dakota.smith@uvm.edu>
2024-01-04 15:08:59 -0500

    Module 10 - added authentication - complete

--- module_10-authentication/server.js
+++ module_10-authentication/server.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 // routes
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
 app.use("/employees", require("./routes/api/employees"));
 
 // app.use('/') for middleware
