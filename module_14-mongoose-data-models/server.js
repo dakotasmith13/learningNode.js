487b26237e6999369a2545d3f061c22b4414fd1f
BuildTools <dakota.smith@uvm.edu>
2024-01-05 18:15:09 -0500

    Module 14 - Mongoose data types initial commit

---
+++ module_14-mongoose-data-models/server.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
+
+const PORT = process.env.PORT || 3500;
+
+// Connect to MongoDB
+connectDB();
+
+// custom middleware logger
+app.use(logger);
+
+// Handle options credentials check - before CORS, fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross origin resource sharing (cors)
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data (form data)
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built in middleware for json
+app.use(express.json());
+
+// middleware for cookies
+app.use(cookieParser());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+// routes
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+
+app.use(verifyJWT); // everything after this line will use verifyJWT
+app.use("/employees", require("./routes/api/employees"));
+
+// app.use('/') for middleware
+// anything that made it here should get 404
+app.all("*", (req, res) => {
+  // for routing
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ err: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+app.use(errorHandler);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+});
