c0e6b91fe1615aa7abee38d6199a6649be235f33
BuildTools <dakota.smith@uvm.edu>
2024-01-05 17:03:47 -0500

    Module 12 - added roles and verification - completed

--- module_12-user-roles/controllers/authController.js
+++ module_12-user-roles/controllers/authController.js
@@ -17,31 +17,44 @@ const handleLogin = async (req, res) => {
   if (!user || !pwd)
     return res
       .status(400)
-      .json({ message: "Username and password are required." });
+      .json({ "message": "Username and password are required." });
   const foundUser = usersDB.users.find((person) => person.username === user);
   if (!foundUser) return res.sendStatus(401); //unauthorized
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
+    const roles = Object.values(foundUser.roles);
     const accessToken = jwt.sign(
-      { username: foundUser.username },
+      {
+        "UserInfo": {
+          "username": foundUser.username,
+          "roles": roles,
+        },
+      },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '30s' }
+      { expiresIn: "30s" }
     );
     const refreshToken = jwt.sign(
-      { username: foundUser.username },
+      { "username": foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: '1d' }
+      { expiresIn: "1d" }
     );
     // saving refreshToken with current user
-    const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-    const currentUser = {...foundUser, refreshToken };  
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
     usersDB.setUsers([...otherUsers, currentUser]);
     await fsPromises.writeFile(
-      path.join(__dirname, '..', 'model', 'users.json'),
+      path.join(__dirname, "..", "model", "users.json"),
       JSON.stringify(usersDB.users)
     );
-    res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000})  
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
     res.json({ accessToken });
   } else {
     res.sendStatus(401); // unauthorized
