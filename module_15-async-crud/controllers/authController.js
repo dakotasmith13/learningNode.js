1240c6d7dcc3719b874b4efa307723338d3ec804
BuildTools <dakota.smith@uvm.edu>
2024-01-05 21:18:41 -0500

    Module 15 - Completed tutorial

--- module_15-async-crud/controllers/authController.js
+++ module_15-async-crud/controllers/authController.js
@@ -1,15 +1,6 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
-
 const jwt = require("jsonwebtoken");
-const fsPromises = require("fs").promises;
-const path = require("path");
 
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
@@ -17,7 +8,7 @@ const handleLogin = async (req, res) => {
     return res
       .status(400)
       .json({ "message": "Username and password are required." });
-  const foundUser = usersDB.users.find((person) => person.username === user);
+  const foundUser = await User.findOne({ username: user }).exec();
   if (!foundUser) return res.sendStatus(401); //unauthorized
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
@@ -39,19 +30,14 @@ const handleLogin = async (req, res) => {
       { expiresIn: "1d" }
     );
     // saving refreshToken with current user
-    const otherUsers = usersDB.users.filter(
-      (person) => person.username !== foundUser.username
-    );
-    const currentUser = { ...foundUser, refreshToken };
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+    console.log(result);
+
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
       sameSite: "None",
-      secure: true,
+      // secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({ accessToken });
--- module_15-async-crud/server.js
+++ module_15-async-crud/server.js
@@ -47,6 +47,7 @@ app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
 
 app.use(verifyJWT); // everything after this line will use verifyJWT
+app.use("/users", require("./routes/api/users"));
 app.use("/employees", require("./routes/api/employees"));
 
 // app.use('/') for middleware
